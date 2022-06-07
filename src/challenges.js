6b52a12a129a616d394c42d1071d0e5bb467ede3
Lucas Lisboa <lucaslisboamotta@gmail.com>
2022-06-07 16:32:52 -0300

    refatorando as funções apos aprender a utilizar as higher order functions

--- src/challenges.js
+++ src/challenges.js
@@ -1,104 +1,49 @@
-function compareTrue(alfa, beta) {
-  return alfa && beta;
-}
+const compareTrue = (alfa, beta) => alfa && beta;
 
-function calcArea(base, altura) {
-  return (base * altura) / 2;
-}
+const calcArea = (base, altura) => (base * altura) / 2;
 
-function splitSentence(frase) {
-  let array = [];
-  let frase2 = '';
-  for (let i = 0; i < frase.length; i += 1) {
-    if (frase[i] === ' ') {
-      array.push(frase2);
-      frase2 = '';
-    } else if (i === frase.length - 1) {
-      frase2 += frase[i];
-      array.push(frase2);
-    } else {
-      frase2 += frase[i];
-    }
-  } return array;
-}
+const splitSentence = (frase) => frase.split(' ');
 
-function concatName(array2) {
-  let frase3 = '';
-  for (let i = 0; i < 3; i += 1) {
-    if (i === 0) {
-      frase3 += array2[array2.length - 1];
-    } else if (i === 1) {
-      frase3 += ', ';
-    } else {
-      frase3 += array2[0];
-    }
-  } return frase3;
-}
+const concatName = (array) => `${array[array.length - 1]}, ${array[0]}`;
 
-function footballPoints(vitoria, empate) {
-  return (vitoria * 3) + empate;
-}
+const footballPoints = (vitoria, empate) => (vitoria * 3) + empate;
 
-function highestCount(s) {
-  return (s.sort((a, b) => b - a).lastIndexOf(s[0]) - s.sort((a, b) => b - a).indexOf(s[0])) + 1;
-}
+const highestCount = (array) => (array.sort((a, b) => b - a)
+  .lastIndexOf(array[0]) - array.sort((a, b) => b - a).indexOf(array[0])) + 1;
 
-function catAndMouse(rato, gato1, gato2) {
+const catAndMouse = (rato, gato1, gato2) => {
   let cat1 = Math.abs(rato - gato1);
   let cat2 = Math.abs(rato - gato2);
-  let cat3 = 'os gatos trombam e o rato foge';
-  if (cat1 < cat2) {
-    cat3 = 'cat1';
-  } else if (cat2 < cat1) {
-    cat3 = 'cat2';
-  } return cat3;
-}
+  if (cat1 < cat2) return 'cat1';
+  if (cat2 < cat1) return 'cat2';
+  return 'os gatos trombam e o rato foge';
+};
 
-function fizzBuzz(array3) {
-  let array4 = [];
-  for (let i = 0; i < array3.length; i += 1) {
-    if (array3[i] % 15 === 0) {
-      array4.push('fizzBuzz');
-    } else if (array3[i] % 3 === 0) {
-      array4.push('fizz');
-    } else if (array3[i] % 5 === 0) {
-      array4.push('buzz');
-    } else {
-      array4.push('bug!');
-    }
-  } return array4;
-}
+const fizzBuzz = (array) => array.map((element) => {
+  if (!(element % 15)) return 'fizzBuzz';
+  if (!(element % 3)) return 'fizz';
+  if (!(element % 5)) return 'buzz';
+  return 'bug!';
+});
 
-function encode(codigo) {
-  let letras = ['a', 'e', 'i', 'o', 'u'];
-  let numeros = ['1', '2', '3', '4', '5'];
-  let codificada = '';
-  for (let i = 0; i < codigo.length; i += 1) {
-    for (let i2 = 0; i2 < 6; i2 += 1) {
-      if (i2 === 5) {
-        codificada += codigo[i];
-      } else if (letras[i2] === codigo[i]) {
-        codificada += numeros[i2];
-        break;
-      }
-    }
-  } return codificada;
-}
-function decode(decodigo) {
-  let letras1 = ['a', 'e', 'i', 'o', 'u'];
-  let numeros1 = ['1', '2', '3', '4', '5'];
-  let codificada1 = '';
-  for (let i = 0; i < decodigo.length; i += 1) {
-    for (let i2 = 0; i2 < 6; i2 += 1) {
-      if (i2 === 5) {
-        codificada1 += decodigo[i];
-      } else if (numeros1[i2] === decodigo[i]) {
-        codificada1 += letras1[i2];
-        break;
-      }
-    }
-  } return codificada1;
-}
+const encode = (codigo) => {
+  const letras = ['a', 'e', 'i', 'o', 'u'];
+  const numeros = ['1', '2', '3', '4', '5'];
+  return codigo.split('').map((letra) => {
+    if (letras.includes(letra)) return numeros[letras.indexOf(letra)];
+    return letra;
+  }).join('');
+};
+
+const decode = (decodigo) => {
+  const letras = ['a', 'e', 'i', 'o', 'u'];
+  const numeros = ['1', '2', '3', '4', '5'];
+  return decodigo.split('').map((letra) => {
+    if (numeros.includes(letra)) return letras[numeros.indexOf(letra)];
+    return letra;
+  }).join('');
+};
+
 module.exports = {
   calcArea,
   catAndMouse,
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,74 +1,46 @@
 // Desafio 10
-function techList(tecnologia, nome) {
-  let listaTec = [];
-  if (tecnologia.length === 0) {
-    return 'Vazio!';
-} else {
-    for (i = 0; i < tecnologia.length; i += 1) {
-        listaTec.push({"name": nome, "tech": tecnologia.sort()[i]})
-   }  return listaTec
-   } 
-}
+const techList = (tecnologia, name) => {
+  if (!tecnologia.length) return 'Vazio!';
+  return tecnologia.sort().map((tech) => ({ name, tech }));
+};
 
-// Desafio 11
-function generatePhoneNumber(telefone) {
-  let seq6 = ''
-  for (i = 0; i < 15; i += 1 ) {
-    if ( i == 0) {
-        seq6 += "("
-    } else if (i >= 1 && i <= 2) {
-        seq6 += telefone[i - 1]
-    } else if (i == 3) {
-        seq6 += ")"
-    } else if (i == 4) {
-        seq6 += " "
-    }else if (i >= 5 && i <= 9) {
-        seq6 += telefone[i - 3]
-    } else if (i == 10) {
-        seq6 += "-"
-    } else {
-        seq6 += telefone[i - 4]
-    } 
-}
-  let seq1 = [...telefone.sort()]
-  let seq2 = []
-  let seq3 = []
-  let seq4 = []
-  let seq5 = []
-  for (i = 0; i < seq1.length; i += 1) {
-      seq2.push(seq1.indexOf(seq1[i]))
-      seq3.push(seq1.lastIndexOf(seq1[i]))
-  } for (i = 0; i < seq1.length; i += 1) {
-      seq4.push((seq3[i] - seq2[i]) + 1)
-  } seq5 = seq4.sort(compararNumeros)[seq4.length - 1]
- if (telefone.length < 11 || telefone.length > 11) {
-    return "Array com tamanho incorreto."
-} else if (seq5 > 2  || telefone.sort(compararNumeros)[0] < 0 || telefone.sort(compararNumeros)[telefone.length - 1] > 9 ) {
-    return "não é possível gerar um número de telefone com esses valores"
-} else {
-    return seq6
-  }
-}
+const generatePhoneNumber = (telefone) => {
+  if (telefone.length !== 11) return 'Array com tamanho incorreto.';
+  const numeroNovo = [...telefone].sort((a, b) => a - b);
+  const maiorNumeroRepetido = numeroNovo.reduce((acumulador, numero) => {
+    acumulador[numero] = acumulador[numero] + 1 || 1;
+    return acumulador;
+  }, {});
+  const numeroRepetido = Object.values(maiorNumeroRepetido).sort((a, b) => b - a)[0];
+
+  if (numeroNovo[0] < 0
+    || numeroNovo.reverse()[0] > 9
+    || numeroRepetido >= 3) return 'não é possível gerar um número de telefone com esses valores';
+
+  telefone.splice(0, 0, '(');
+  telefone.splice(3, 0, ')');
+  telefone.splice(4, 0, ' ');
+  telefone.splice(10, 0, '-');
+  return telefone.join('');
+};
 
 // Desafio 12
-function triangleCheck(ladoA, ladoB, ladoC) {
-  return (ladoB + ladoC) - ladoA > 0 && (ladoA + ladoC) - ladoB > 0 && (ladoA + ladoC) - ladoC > 0;
-}
+const triangleCheck = (ladoA, ladoB, ladoC) => (
+  (ladoB + ladoC) - ladoA > 0
+  && (ladoA + ladoC) - ladoB > 0
+  && (ladoA + ladoC) - ladoC > 0
+);
 
 // Desafio 13
 function hydrate(bebidas) {
-  let numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-  let agua = 0;
-  let unica = '1 copo de água';
-  for (let i = 0; i < bebidas.length; i += 1) {
-    for (let i2 = 0; i2 < 9; i2 += 1) {
-      if (bebidas[i] === numeros[i2]) {
-        agua += i2 + 1;
-      }
-    }
-  } if (agua !== 1) {
-    unica = numeros[agua - 1] + ' copos de água';
-  } return unica;
+  const numeros = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const agua = bebidas.split('').reduce((acumulador, letra) => {
+    if (numeros.includes(letra)) return acumulador + Number(letra);
+    return acumulador;
+  }, 0);
+
+  if (agua > 1) return `${agua} copos de água`;
+  return '1 copo de água';
 }
 
 module.exports = {
@@ -77,30 +49,3 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
-
-// utilizei a função para orgarnizar os numeros do array em ordem crescente na prorpiedade .sort()
-// link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-function compararNumeros(a, b) {
-  return a - b;
-}
-// função para separar converter os numeros em string, atividade 11
-function convertNumero(arrayN) {
-  let seq6 = ''
-  for (i = 0; i < 15; i += 1 ) {
-    if ( i == 0) {
-        seq6 += "("
-    } else if (i >= 1 && i <= 2) {
-        seq6 += arrayN[i - 1]
-    } else if (i == 3) {
-        seq6 += ")"
-    } else if (i == 4) {
-        seq6 += " "
-    }else if (i >= 5 && i <= 9) {
-        seq6 += arrayN[i - 3]
-    } else if (i == 10) {
-        seq6 += "-"
-    } else {
-        seq6 += arrayN[i - 4]
-    }
-} return seq6;
-}
